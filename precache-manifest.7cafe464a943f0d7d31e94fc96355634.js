c613d3b8b3cc0d10ee3cabc6f52d49508a0bc507
gestap <gestap9@gmail.com>
2020-02-19 10:44:52 +0200

    Updates

---
+++ precache-manifest.7cafe464a943f0d7d31e94fc96355634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373d7a6812a35cb2d6eeeb08d41b7742",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27bed3578b4ff63feb80",
+    "url": "./static/css/main.bd8c1f51.chunk.css"
+  },
+  {
+    "revision": "18308e7acefbb10f7eb2",
+    "url": "./static/js/2.328022cc.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "./static/js/2.328022cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bed3578b4ff63feb80",
+    "url": "./static/js/main.760f23de.chunk.js"
+  },
+  {
+    "revision": "1d4ec920e691b0a42b40",
+    "url": "./static/js/runtime-main.a78a46d1.js"
+  }
+]);
